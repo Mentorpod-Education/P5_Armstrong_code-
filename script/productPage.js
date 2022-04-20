480e0a5fd3faad4f6333a5df78aa61301a331fa8
Donavan <donavan.mc@gmail.com>
2022-04-20 15:30:11 -0700

    u

--- script/productPage.js
+++ script/productPage.js
@@ -62,60 +62,84 @@ function getPost(element) {
 
         // adding products to the cart
         function addToCart(element) {
-            const basket = document.querySelector("#addToCart");
+            const basket = document.querySelectorAll("#addToCart");
             const colorChoice = color.value;
             const quantityChoice = quantity.value;
+
+            let retrieveOptions = {
+                productId: idProduct,
+                image: element.imageUrl,
+                altProduct: element.altTxt,
+                name: element.name,
+                price: element.price,
+                description: element.description,
+                colorOption: colorChoice,
+                quantityOption: quantityChoice,
+            }
         
-         
-            basket.addEventListener("click", (event) => { 
-                if (quantityChoice.value > 0 && quantityChoice.value <= 100) {
         
-                    let retrieveOptions = {
-                        productId: idProduct,
-                        image: element.imageUrl,
-                        altProduct: element.altTxt,
-                        name: element.name,
-                        price: element.price,
-                        description: element.description,
-                        colorOption: colorChoice,
-                        quantityOption: quantityChoice,
-                    }
+            for (let i = 0; i < basket.length; i++) {
+                basket[i].addEventListener("click", () => { 
+                    basketNumbers()
+                    if (quantityChoice.value > 0 && quantityChoice.value <= 100) {
                 
-           
-                    let productLocalStorage = JSON.parse(localStorage.getItem("product"));
-            
-                    const popUpConfirmation = () => {
-                        if (window.confirm (`Your order of ${quantityChoice}  ${element.name}  ${colorChoice} is added to the cart
-                    To view your cart, click OK`)) {
-                            window.location.href ="cart.html";
+                        const popUpConfirmation = () => {
+                            if (window.confirm (`Your order of ${quantityChoice}  ${element.name}  ${colorChoice} is added to the cart
+                        To view your cart, click OK`)) {
+                                window.location.href ="cart.html";
+                            }
                         }
-                    }
+                        
+                    //Import elements into local storage
+                    if (productLocalStorage) {
+                        const resultFind = productLocalStorage.find(
+                            (el) => el.productId === idProduct && el.colorOption === colorChoice);
                     
-                //Import elements into local storage
-                if (productLocalStorage) {
-                    const resultFind = productLocalStorage.find(
-                        (el) => el.productId === idProduct && el.colorOption === colorChoice);
-                 
-                if (resultFind) {
-                    let newQuantity = parseInt(retrieveOptions.quantityOption) + parseInt(resultFind.quantityOption);
-                    resultFind.quantityOption = newQuantity;
-                    localStorage.setItem("product", JSON.stringify(productLocalStorage));
-                    popUpConfirmation();
+                    if (resultFind) {
+                        let newQuantity = parseInt(retrieveOptions.quantityOption) + parseInt(resultFind.quantityOption);
+                        resultFind.quantityOption = newQuantity;
+                        localStorage.setItem("product", JSON.stringify(productLocalStorage));
+                        popUpConfirmation();
 
-                } else {
-                    productLocalStorage.push(retrieveOptions);
-                    localStorage.setItem("product", JSON.stringify(productLocalStorage));
-                    popUpConfirmation();
-                }
+                    } else {
+                        productLocalStorage.push(retrieveOptions);
+                        localStorage.setItem("product", JSON.stringify(productLocalStorage));
+                        popUpConfirmation();
+                    }
 
-                } else {
-                    productLocalStorage = [];
-                    productLocalStorage.push(retrieveOptions);
-                    localStorage.setItem("product", JSON.stringify(productLocalStorage));
-                    popUpConfirmation();
-                }}
-          });
+                    } else {
+                        productLocalStorage = [];
+                        productLocalStorage.push(retrieveOptions);
+                        localStorage.setItem("product", JSON.stringify(productLocalStorage));
+                        popUpConfirmation();
+                    }}
+                });
+            }
+
+            
+
         }
+
+        function addCartNumbers() {
+            let numbers = localStorage.getItem('basketNumbers');
+            if (numbers) {
+                document.querySelector('.cart span').textContent = numbers;
+            }
+        }
+
+        function basketNumbers() {
+            let numbers = localStorage.getItem('basketNumbers');
+            numbers = parseInt(numbers);
+
+            if (numbers) {
+                localStorage.setItem("basketNumbers", numbers + 1);
+            } else {
+                localStorage.setItem("basketNumbers", 1);
+            }
+        }
+
+        addCartNumbers()
+        
 
         addToCart(element);
         console.log(addToCart(element));
